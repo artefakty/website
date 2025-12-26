6ce8feb21f4068404118cee5afb20c0fd5faf580
kuba11g <kubagorzadek@gmail.com>
2025-12-26 16:36:26 +0100

    snowflake effect only for winter time ;)

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from 'react';
 import Navbar from '../layout/Navbar';
 import Footer from '../layout/Footer';
 import styles from '../../styles/MainLayout.module.css';
+import { Snowfall } from '@namnguyenthanhwork/react-snowfall-effect';
 
 interface MainLayoutProps {
   children: ReactNode;
@@ -11,6 +12,12 @@ const MainLayout = ({ children }: MainLayoutProps) => {
   return (
     <div className={styles.layout}>
       <Navbar />
+      <Snowfall
+        snowflakeCount={200}
+        speed={{ min: 1.5, max: 4 }}
+        size={{ min: 12, max: 30 }}
+        snowflakeShape='dot'
+      />
       <main className={styles.content}>
         {children}
       </main>

a55c96eef9092154b8b1d70d1ee40e16157f57a1
kuba11g <kubagorzadek@gmail.com>
2025-12-27 16:00:14 +0100

    uploaded projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,3 +1,11 @@
+import imgFortress from '../assets/images/projects-images/fortress.jpg';
+import imgBrozard from '../assets/images/projects-images/brozard.jpg';
+import imgSmokenSails from '../assets/images/projects-images/smokensails.jpg';
+import imgMirrorDash from '../assets/images/projects-images/mirrordash.jpg';
+import imgTheophiuchus from '../assets/images/projects-images/theophiuchus.jpg';
+import imgThelosttable from '../assets/images/projects-images/thelosttable.jpg';
+import imgUnipol from '../assets/images/projects-images/unipol.jpg';
+
 export type Project = {
   id: string
   title: string
@@ -10,44 +18,90 @@ export type Project = {
   date: string
 }
 
-const placeholderImage = '/assets/images/avatar.svg'
-
 export const projects: Project[] = [
   {
-    id: 'mirror-dash',
-    title: 'MIRROR DASH',
+    id: 'unipol',
+    title: 'Unipol',
     description:
-      'Gra została stworzona na 23h WFIIS GameJaaaaaaa  aaaaaaaa a dad sa sadasasdasd da sad asda da aaaaa aaaaaadsa aaaaaa  aaaa aaa aaaaaaa aad sad asdassad  sadaaaaa   aaaaaaaaaaaa  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaam 2024. Gra dla 2 graczy na podzielonym ekranie. Biegaj i buduj! Pamiętaj, żeby nie zostać zamienionym w stół!',
-    status: 'GRA Z GAME JAMU',
-    genre: 'RACING',
-    authors: 'Jan Kowalski, Anna Nowak, Jan Kowalski, Anna Nowak',
-    projectLink: '/projects/mirror-dash',
-    image: placeholderImage,
-    date: '2024-12-11',
+      '',
+    status: 'GRA',
+    genre: 'Monopoly-like',
+    authors: 'Tobkubos',
+    projectLink: 'https://tobkubos.itch.io/unipol',
+    image: imgUnipol,
+    date: '2025-05-14',
   },
   {
-    id: 'darkness-rts',
-    title: 'Darkness',
+    id: 'thelosttable',
+    title: 'The Lost Table',
     description:
-      'To nowy projekt rozwijany w czasie wolnym, w którym z głębokiej ciekawości zanurkowałem w Deterministic Lockstep.',
-    status: 'WORK-IN-PROGRESS',
-    genre: 'RTS',
-    authors: 'Jan Kowalski',
-    projectLink: '',
-    image: placeholderImage,
-    date: '2024-11-05',
+      'The Lost Table to platformówka, w której celem gracza jest odnalezienie zaginionych nóg stołu. Droga do sukcesu wymaga planowania i używania magicznej piłki, zdolnej do modyfikacji terenu. Piłka potrafi odbijać się od elementów mapy, co otwiera dodatkowe możliwości manipulowania otoczeniem i rozwiązywania zagadek. Po planszy latają jednak duchy, które potrafią zabierać poszczególne rodzaje magicznych piłek, co dodatkowo utrudnia rozgrywkę. Myśl strategicznie, wykorzystuj odbicia i działaj z refleksem, aby odzyskać nogi stołu i unikać przeszkadzających upiorów!',
+    status: 'GRA, Game Jam',
+    genre: 'Platformer',
+    authors: 'BearNerd',
+    projectLink: 'https://bearnerd.itch.io/lost-table',
+    image: imgThelosttable,
+    date: '2025-05-12',
   },
   {
-    id: 'darkness-rts-game',
-    title: 'Darkness',
+    id: 'theophiuchus',
+    title: 'The Ophiuchus',
     description:
-      'To nowy projekt rozwijany w czasie wolnym, w którym z głębokiej ciekawości zanurkowałem w Deterministic Lockstep.',
-    status: 'WORK-IN-PROGRESS',
-    genre: 'RTS, Game',
-    authors: 'Jan Kowalski',
-    projectLink: '',
-    image: placeholderImage,
-    date: '2024-11-05',
+      'Gra, w której gracz wciela się w tytułowego "Wężownika" , trzynasty znak zodiaku. Odrzucony przez pozostałą dwunastkę musi walczyć o wolność,  mimo że jego los jest z góry przesądzony...',
+    status: 'GRA, Game Jam',
+    genre: 'Shooter',
+    authors: 'Tobkubos, kuba11g, Kassjan, Gradi, BearNerd',
+    projectLink: 'https://tobkubos.itch.io/the-ophiuchus',
+    image: imgTheophiuchus,
+    date: '2025-04-27',
+  },
+  {
+    id: 'mirrordash',
+    title: 'Mirror DASH!',
+    description:
+      'Commision for 23h WFIIS GameJam. Game for 2 players on split screen. Run and Build! Remember to not be transformed into table!',
+    status: 'GRA, Game Jam',
+    genre: 'Racing',
+    authors: 'Tobkubos, kuba11g, Kassjan, Gradi',
+    projectLink: 'https://tobkubos.itch.io/mirror-dash',
+    image: imgMirrorDash,
+    date: '2024-10-19',
+  },
+  {
+    id: 'smokensails',
+    title: 'Smoke\'n\'Sails',
+    description:
+      'Smoke\'n\'Sails is a roguelike game, where the player takes on the role of a wizard who fights against slimes. After each wave, the player has to allocate points to improve his battle skills. Additionally, he gets powerful artifacts every few waves. There is also a chance of a thunderstorm, which will reduce visibility. You can encounter up to 5 different slimes and their mutated versions as well!',
+    status: 'GRA, Game Jam',
+    genre: 'Shooter',
+    authors: 'Tobkubos, kuba11g, Kassjan, Gradi, mikeAl0ne',
+    projectLink: 'https://tobkubos.itch.io/smokesnsail-szlifierka-ktowa-bosch',
+    image: imgSmokenSails,
+    date: '2024-03-11',
+  },
+  {
+    id: 'brozard',
+    title: 'BROzard',
+    description:
+      'BROzard is a roguelike game, where the player takes on the role of a wizard who fights against slimes. After each wave, the player has to allocate points to improve his battle skills. Additionally, he gets powerful artifacts every few waves. There is also a chance of a thunderstorm, which will reduce visibility. You can encounter up to 5 different slimes and their mutated versions as well!',
+    status: 'GRA',
+    genre: 'Survival',
+    authors: 'Tobkubos, Gradi',
+    projectLink: 'https://tobkubos.itch.io/brozard',
+    image: imgBrozard,
+    date: '2023-10-21',
+  },
+  {
+    id: 'fortress',
+    title: 'Fortress',
+    description:
+      'A game by HotPotato team',
+    status: 'GRA',
+    genre: 'STRATEGY, CARD GAME, SURVIVAL',
+    authors: 'Piotr Szekalski (Lead / Art), Piotr Kosek (Coding), Grażyna Pietruszka (Art), Sebastian Brasiak (Art)',
+    projectLink: 'https://shelim.itch.io/fortress',
+    image: imgFortress,
+    date: '2022-07-22',
   },
 ]
 
